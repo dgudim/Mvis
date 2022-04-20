d234cafa2031515b1730b57cc320c3dd32093e1b
dgudim <dgudim@gmail.com>
2022-04-20 19:46:00 +0300

    display stuff

--- android/src/com/deo/mvis/AndroidLauncher.java
+++ android/src/com/deo/mvis/AndroidLauncher.java
@@ -1,9 +1,16 @@
 package com.deo.mvis;
 
+import static com.deo.mvis.Launcher.HEIGHT;
+import static com.deo.mvis.Launcher.WIDTH;
+
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Insets;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.WindowInsets;
+import android.view.WindowMetrics;
 
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
@@ -18,6 +25,20 @@ public class AndroidLauncher extends AndroidApplication {
         config.useGyroscope = false;
         config.hideStatusBar = true;
         config.useWakelock = true;
+        
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            WindowMetrics windowMetrics = getWindowManager().getCurrentWindowMetrics();
+            Insets insets = windowMetrics.getWindowInsets()
+                    .getInsetsIgnoringVisibility(WindowInsets.Type.systemBars());
+            WIDTH = windowMetrics.getBounds().width() - insets.left - insets.right;
+            HEIGHT = windowMetrics.getBounds().height() - insets.top - insets.bottom;
+        } else {
+            DisplayMetrics displayMetrics = new DisplayMetrics();
+            getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+            WIDTH = displayMetrics.widthPixels;
+            HEIGHT = displayMetrics.heightPixels;
+        }
+        
         initialize(new Launcher(), config);
         if (this.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
             this.requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
